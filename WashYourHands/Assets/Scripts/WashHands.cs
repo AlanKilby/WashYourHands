02fea7d4418881010e884ee86908a15b898c1010
Alan Kilby <alan.kilby.cerquera@gmail.com>
2020-10-08 17:00:47 +0200

    Wash Hand Script
    Added a script that allows you wash your hands.
    Added test sprite.

---
+++ WashYourHands/Assets/Scripts/WashHands.cs
@@ -0,0 +1,76 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WashHands : MonoBehaviour
+{
+    public GameObject upperHand;
+    public GameObject lowerHand;
+
+    public float handOffset;
+
+    public float timer;
+    private float timerHolder;
+
+    public float score;
+
+    private bool movedUpper;
+    private bool movedLower;
+
+    void Start()
+    {
+        movedUpper = false;
+        movedLower = false;
+
+        score = 0;
+        timerHolder = timer;
+
+    }
+
+    void Update()
+    {
+        if(timer > 0)
+        {
+            timer -= Time.deltaTime;
+            if (Input.anyKeyDown)
+            {
+                score++;
+                MoveHands();
+            }
+        }
+
+        if(timer == 0)
+        {
+            Debug.Log(score);
+        }
+    }
+
+    public void MoveHands()
+    {
+        if (movedUpper == false)
+        {
+            Debug.Log("This happened");
+            upperHand.transform.position = new Vector2(upperHand.transform.position.x, handOffset);
+            movedUpper = true;
+        }
+        else if(movedUpper == true)
+        {
+            upperHand.transform.position = new Vector2(upperHand.transform.position.x, -handOffset);
+            movedUpper = false;
+        }
+
+        if (movedLower == false)
+        {
+            lowerHand.transform.position = new Vector2(lowerHand.transform.position.x, -handOffset);
+            movedLower = true;
+        }
+        else if (movedLower == true)
+        {
+            lowerHand.transform.position = new Vector2(lowerHand.transform.position.x, handOffset);
+            movedLower = false;
+        }
+    }
+
+    
+}
